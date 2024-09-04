7a742dcac085720e8109f14c53c4c758f973d60c
ghtjr410 <ghtjr410@gmail.com>
2024-09-04 17:46:26 +0900

    front 생성 - React 설치 - TypeScript 설치 - TailWind 설치

---
+++ front/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import MainPage from './page/homepage';
+
+
+function App() {
+  return (
+    <div className="App">
+      <MainPage></MainPage>
+    </div>
+  );
+}
+
+export default App;
