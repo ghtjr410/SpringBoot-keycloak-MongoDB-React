8df4e68acdeb9997f0bbd60d20ebbca3910f5cba
ghtjr410 <ghtjr410@gmail.com>
2024-09-05 00:39:56 +0900

    front - 로그인 버튼 생성 - 로그아웃 버튼 생성 - 계정관리 버튼 생성

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -2,12 +2,26 @@ import React from 'react';
 import Public from './components/Public';
 import Private from './components/Private';
 import useAuth from './hooks/useAuth';
+import keycloak from './auth/keyCloak';
 
 
 function App() {
-  const isLogin = useAuth();
+  const isSignin = useAuth();
+  
   return (    
-      isLogin ? <Private /> : <Public/>
+    <div className='flex flex-col bg-slate-500'>
+    {isSignin ? (
+      <>
+        <button onClick={() => keycloak.logout()}>로그아웃</button>
+        <button onClick={() => keycloak.accountManagement()}>
+            계정 관리
+          </button>
+      </>
+      
+    ) : (
+        <button onClick={() => keycloak.login()}>로그인</button>
+    )}
+</div>
   );
 }
 
